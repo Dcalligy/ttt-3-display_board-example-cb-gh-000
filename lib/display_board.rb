15a6898ff87c29d6057e25077041890bd689a04b
Dillon Calligy <dcalligy@mail.usf.edu>
2018-11-15 14:40:41 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,5 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  print "Welcome to Tic Tac Toe!\n"
   print "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n"
 end
 display_board # invokes the method
